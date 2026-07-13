f4dbe66ae47c1974954a2c259727b4918e5fb2c2
Danny <danny.koshik@gmail.com>
2026-07-13 19:18:26 +0300

    fix(compilation): исправил обновление списка событий в подборках

--- core/compilation-service/src/main/java/ru/practicum/compilation/service/CompilationService.java
+++ core/compilation-service/src/main/java/ru/practicum/compilation/service/CompilationService.java
@@ -39,23 +39,27 @@ public class CompilationService {
     }
 
     @Transactional
-    public CompilationDto update(Long compId, UpdateCompilationRequest dto) {
+    public CompilationDto update(Long compId, UpdateCompilationRequest request) {
         Compilation compilation = compilationRepository.findById(compId)
+                .orElseThrow(() -> new NotFoundException("Подборка с id=" + compId + " не найдена"));
+
+        if (request.getPinned() != null) {
+            compilation.setPinned(request.getPinned());
+        }
+
+        if (request.getTitle() != null && !request.getTitle().isBlank()) {
+            compilation.setTitle(request.getTitle());
+        }
+
+        if (request.getEvents() != null) {
+            compilation.setEventIds(new LinkedHashSet<>(request.getEvents()));
+        }
+
+        compilationRepository.saveAndFlush(compilation);
+
+        Compilation detailed = compilationRepository.findDetailedById(compId)
                 .orElseThrow(() -> new NotFoundException("Compilation with id=" + compId + " was not found"));
 
-        if (dto.getEvents() != null) {
-            compilation.setEventIds(new LinkedHashSet<>(dto.getEvents()));
-        }
-        if (dto.getPinned() != null) {
-            compilation.setPinned(dto.getPinned());
-        }
-        if (dto.getTitle() != null) {
-            compilation.setTitle(dto.getTitle());
-        }
-
-        compilationRepository.save(compilation);
-        Compilation detailed = compilationRepository.findDetailedById(compilation.getId())
-                .orElseThrow(() -> new NotFoundException("Compilation with id=" + compilation.getId() + " was not found"));
         return compilationMapper.toDto(detailed);
     }
 
