9e7de2195366e7ade32ca3b3bb1e6005c98d0178
Danny <danny.koshik@gmail.com>
2026-07-14 04:37:32 +0300

    fix: handle invalid date parameters in stats-service

--- stats-service/server/src/main/java/ru/practicum/stats/exception/ErrorHandler.java
+++ stats-service/server/src/main/java/ru/practicum/stats/exception/ErrorHandler.java
@@ -1,11 +1,15 @@
 package ru.practicum.stats.exception;
 
+import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 
 @RestControllerAdvice
 public class ErrorHandler {
@@ -20,15 +24,17 @@ public class ErrorHandler {
 //                LocalDateTime.now());
 //    }
 
-    @ExceptionHandler(Throwable.class)
+    @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiError handleGeneralError(Throwable e) {
         return new ApiError("INTERNAL_SERVER_ERROR", "Произошла непредвиденная ошибка", e.getMessage(), LocalDateTime.now());
     }
 
     @ExceptionHandler({
-            org.springframework.web.bind.MissingServletRequestParameterException.class,
-            org.springframework.web.method.annotation.MethodArgumentTypeMismatchException.class
+            MissingServletRequestParameterException.class,
+            MethodArgumentTypeMismatchException.class,
+            DateTimeParseException.class,
+            ConversionFailedException.class
     })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiError handleParamErrors(Exception e) {
